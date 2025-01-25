65dba580a067e9977ffb296d1a0aa1a491ff2dc3
código4edutech <ibt.centralcontato@gmail.com>
2025-01-25 20:16:09 -0300

    Separando os arquivos em HTML, CSS e JavaScript

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Se você quiser adicionar alguma funcionalidade interativa ao menu ou página,
+// você pode incluir o código JavaScript aqui.
+
+console.log("Menu Moderno e Minimalista carregado com sucesso!");
